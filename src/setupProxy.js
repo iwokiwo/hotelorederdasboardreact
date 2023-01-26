163a8970b20028ba9948df851ffa3c14e8c64a75
bayu <bayu@ipaymu.com>
2023-01-26 21:51:17 +0800

    proxy

---
+++ src/setupProxy.js
@@ -0,0 +1,9 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+module.exports = (app) => {
+    app.use(createProxyMiddleware('/api/v1/sessions', {
+        target: 'http://103.250.10.196:8090',
+        changeOrigin: true,
+        // pathRewrite: { '^/api2': '' }
+    })
+    );
+}
